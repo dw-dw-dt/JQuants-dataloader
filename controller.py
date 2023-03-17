cb71b89c1e0cf573d97f64ed356ad8b5f265ee3a
Daiya Mita <18240230+dw-dw-dt@users.noreply.github.com>
2023-03-17 14:08:16 +0900

    popen -> run

--- controller.py
+++ controller.py
@@ -9,8 +9,7 @@ from src.utils import FILE_PATH
 if __name__ == "__main__":
     # 1-shot code 
     # for script in ['src/trade_info_loader.py', 'src/index_price_loader.py', 'src/fin_announcement_loader.py']:
-    #     res = subprocess.Popen(['python', script])
-    #     res.wait()
+    #     res = subprocess.run(['python', script])
 
     # loop code 1
     from_date = dt.datetime(2017,1,4).strftime('%Y%m%d')
@@ -22,8 +21,7 @@ if __name__ == "__main__":
         listed_security_info = pd.read_csv(f'{FILE_PATH}/listed_info/20230316.csv')
         for security_code in tqdm(listed_security_info.query('Sector33Code!=9999')['Code']):
             try:
-                res = subprocess.Popen(['python', script, str(security_code)])
-                res.wait()
+                res = subprocess.run(['python', script, str(security_code)])
             except Exception as e:
                 print(security_code, e)
             time.sleep(1)
